9995604ee516d31a7bff14be1edc922e3d649e5d
FirefoxMetzger <sebastian@wallkoetter.net>
2017-02-08 13:30:01 +0000

    added a header to keep all the pin definitions

---
+++ source/main/initio.h
@@ -0,0 +1,39 @@
+// special pins for the arduino and initio buggy
+
+// special ports used by the arduino
+#define TLL_RX 0
+#define TLL_TX 1
+
+#define INTERRUPT_A 2
+#define INTERRUPT_B 3
+
+#define PWM_A 6
+#define PWM_B 9
+#define PWM_C 10
+#define PWM_D 11
+#define PWM_E 3
+#define PWM_F 5
+
+#define SPI_SS 10
+#define SPI_MOSI 11
+#define SPI_MISO 12
+#define SPI_SCK 13
+
+#define LED 13
+
+#define TWI_SDA A4
+#define TWI_SCL A5
+
+// special ports used by the initio
+#define MOTOR_A_DIRECTION 12
+#define MOTOR_A_PWM	3
+#define MOTOR_A_BREAK 9
+#define MOTOR_A_CURRENT_SENSING A0
+
+#define MOTOR_B_DIRECTION 13
+#define MOTOR_B_PWM	11
+#define MOTOR_B_BREAK 8
+#define MOTOR_B_CURRENT_SENSING A1
+
+#define ENCODER_A 6
+#define ENCODER_B 5
