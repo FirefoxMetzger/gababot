af5a96db5a3da98985594eaf368140a5b1bff214
FirefoxMetzger <sebastian@wallkoetter.net>
2017-02-08 13:38:27 +0000

    minimal wheel turning in main.ino
    Wrote a small program, that spins one wheel in order to test, if MOTOR_A
    is the left motor. It is indeed.

--- source/main/main.ino
+++ source/main/main.ino
@@ -1,9 +1,27 @@
+#include "initio.h"
+
 void setup()
 {
+  //configure the motor pins
+  pinMode(MOTOR_A_DIRECTION,OUTPUT);
+  pinMode(MOTOR_A_PWM,OUTPUT);
+  pinMode(MOTOR_A_BREAK,OUTPUT);
+  pinMode(MOTOR_A_CURRENT_SENSING,INPUT);
 
+  pinMode(MOTOR_B_DIRECTION,OUTPUT);
+  pinMode(MOTOR_B_PWM,OUTPUT);
+  pinMode(MOTOR_B_BREAK,OUTPUT);
+  pinMode(MOTOR_B_CURRENT_SENSING,INPUT);
+
+  digitalWrite(MOTOR_A_DIRECTION,HIGH);
+  digitalWrite(MOTOR_A_BREAK,LOW);
+  analogWrite(MOTOR_A_PWM,100);
 }
 
 void loop()
 {
-
+  delay(1000);
+  digitalWrite(MOTOR_A_DIRECTION,LOW);
+  delay(1000);
+  digitalWrite(MOTOR_A_DIRECTION,HIGH);
 }
