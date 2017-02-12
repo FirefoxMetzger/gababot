b6acad0609f0434493d8b1f584a73111731d481b
FirefoxMetzger <sebastian@wallkoetter.net>
2017-02-12 16:08:58 +0000

    continued serial interface

--- source/main/SerialCommunication.h
+++ source/main/SerialCommunication.h
@@ -3,9 +3,23 @@ namespace SOFT561
 {
   namespace Arduino
   {
+    enum SerialMessage {
+      up,
+      down,
+      left,
+      right
+    };
     class SerialCommunication
     {
-      
+      private:
+        SerialMessage _in_message;
+        SerialMessage _out_message;
+        
+      public:
+        SerialCommunication(int baud);
+        
+        void push(SerialMessage msg);
+        SerialMessage pop();
     };
   }
 }
