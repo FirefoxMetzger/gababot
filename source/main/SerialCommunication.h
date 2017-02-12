9c7e54ecb4af58f0668c4f16c66f925cc9526354
FirefoxMetzger <sebastian@wallkoetter.net>
2017-02-12 19:42:08 +0000

    added implementation for SerialCommunication

--- source/main/SerialCommunication.h
+++ source/main/SerialCommunication.h
@@ -4,11 +4,17 @@ namespace SOFT561
   namespace Arduino
   {
     enum SerialMessage {
-      up,
-      down,
-      left,
-      right
+      none=0,
+      up=1,
+      down=2,
+      left=3,
+      right=4
     };
+
+    enum BaudRates {
+      
+    };
+    
     class SerialCommunication
     {
       private:
@@ -20,6 +26,7 @@ namespace SOFT561
         
         void push(SerialMessage msg);
         SerialMessage pop();
+        void update();
     };
   }
 }
