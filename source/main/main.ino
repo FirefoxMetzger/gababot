cc31ffa7752dfe53a71f2638504c107a5db836e1
FirefoxMetzger <sebastian@wallkoetter.net>
2017-02-12 19:42:29 +0000

    main now testing SerialCommunication

--- source/main/main.ino
+++ source/main/main.ino
@@ -2,15 +2,11 @@
 #include "initio.h"
 #include "motor.h"
 #include "MotorControl.h"
+#include "SerialCommunication.h"
 
 void setup()
 {
   pinMode(LED, OUTPUT);
-}
-
-void whatever()
-{
-
 }
 
 void loop()
@@ -18,22 +14,14 @@ void loop()
   SOFT561::Arduino::Motor* motor_A = new SOFT561::Arduino::Motor(MOTOR_A_DIRECTION, MOTOR_A_PWM, MOTOR_A_BREAK, MOTOR_A_CURRENT_SENSING);
   SOFT561::Arduino::Motor* motor_B = new SOFT561::Arduino::Motor(MOTOR_B_DIRECTION, MOTOR_B_PWM, MOTOR_B_BREAK, MOTOR_B_CURRENT_SENSING);
 
-  motor_A->setDirection(SOFT561::Arduino::forward);
-  motor_B->setDirection(SOFT561::Arduino::backward);
-
-  int speed_counter = 0;
-  int speed_inc = 1;
+  SOFT561::Arduino::SerialCommunication com = SOFT561::Arduino::SerialCommunication(9600);
 
   while (true)
   {
-    motor_A->setSpeed(speed_counter);
-    motor_B->setSpeed(speed_counter);
-
-    if (speed_counter > 255 || speed_counter < 0)
-    {
-      speed_inc *= -1;
-    }
-    speed_counter += speed_inc;
-    delay(100);
+    SOFT561::Arduino::SerialMessage foo = com.pop();
+    com.push(foo);
+    com.update();
+    
+    delay(1000);
   }
 }
