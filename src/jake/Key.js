27afb0096f775d83e36e6c6c602c40e8546f15ac
Sebastian Wallkoetter <sebastian@wallkoetter.net>
2017-03-02 22:47:29 +0000

    started implementing JS keyboard
    created a JS version of the ros message as class. Further, added
    methods to easily add, remove and update the object.
    
    The idea is to use JSON.stringify() to create a json string, cast that
    to a ROS message and then send it to rosbridge.

---
+++ src/jake/Key.js
@@ -0,0 +1,10 @@
+
+class Key 
+{
+	constructor(key)
+	{
+		this.code = key.code;
+		this.key = key.key;
+		this.location = key.location;
+	}
+}
---
+++ src/jake/Keyboard.js
@@ -0,0 +1,78 @@
+class KeyboardState
+{
+	constructor()
+	{
+		this.seq = Date.now();
+		this.last_pressed_idx = -1;
+		this.keydown_length = 0;
+		this.keydown = [];	
+	}
+
+	keydown(key)
+	{
+		var key_tester = this.inList(key);
+		this.seq = Date.now();
+
+		if (key_tester.exists)
+		{
+			this.last_pressed_idx = key_tester.idx;
+		}
+		else
+		{
+			this.last_pressed_idx = this.keydown_length;
+			this.keydown_length += 1;
+			this.keydown.push(new Key(key))
+		}
+	}
+
+	keyup(key)
+	{
+		var key_tester = this.inList(key);
+		if (key_tester.exists)
+		{
+			this.keydown.splice(key_tester.idx,1);
+			this.keydown_length -= 1;
+		}
+		else
+		{
+			console.debug("Tried to remove nonexistant key.")
+		}
+	}
+
+	inList(key)
+	{
+		for (var i = 0; i < this.keydown_length; ++i)
+		{
+			if(assertKey(key,this.keydown[i]))
+			{
+				return {
+					exists:true,
+					idx:i,
+				};
+			}	
+		}	
+		return {exists:false};
+	}
+
+	assertKeys(key1,key2)
+	{
+		// technically I have to check both ways
+		// however I know that I pass in the class attribute as
+		// a second value, this I can save one loop
+		for(value property in key2)
+		{
+			if 
+			(
+				key1.hasOwnProperty(property) &&
+				key2.hasOwnProperty(property)
+			)
+			{
+				if(key1.property !== key2.property) 
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+}
