0a02303a27be48bbea132214c7bdc4f5efb9dae0
Sebastian Wallkoetter <sebastian@wallkoetter.net>
2017-05-10 18:50:44 +0100

    added comments

--- src/web_interface/webcam_image_callback.js
+++ src/web_interface/webcam_image_callback.js
@@ -1,4 +1,12 @@
+// Author: Sebastian Wallkötter
+// Email: sebastian@wallkoetter.net
+// License: MIT
 
+// this file depends on:
+// - setup_ROS.js
+// it is included in index.html (because browsers don't yet support import)
+
+// The callback executed whenever a new image is published in ROS
 function webcam_stream_callback(message)
 {
     //var canvas = document.getElementById("webcam");
@@ -10,4 +18,6 @@ function webcam_stream_callback(message)
     document.getElementById('webcam').setAttribute('src', imagedata);
 }
 
+//webcam_topic is a global variable defined in setup_ROS
+// -- there should be a better way for this
 webcam_topic.subscribe(webcam_stream_callback);
